2ecbb40047066b964661924553ee70d2009aec29
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2021-10-26 14:19:42 -0400

    sockets

--- server/index.js
+++ server/index.js
@@ -18,7 +18,18 @@ function send(res, file) {
 }
 
 socket.on('connection', (socket) => {
-    console.log('new connection')
+    let url = socket.handshake.query.url.split('/')[3]
+    clients[url].push(socket.id)
+    socket.broadcast.emit('player connect')
+
+    socket.on('move', (board) => { // when client emits a change to the board
+        socket.broadcast.emit('move', {board: board}) // server emits to other clients
+    })
+
+    socket.on('disconnect', () => {
+        clients[url].splice(clients[url].indexOf(socket.id), 1)
+        socket.broadcast.emit('player disconnect')
+    })
 })
 
 server.get('/', (req, res) => {
@@ -28,11 +39,9 @@ server.get('/', (req, res) => {
 server.get('/:room', (req, res) => {
     let roomID = req.params.room
     if (req.params.room in clients) { // DEFINITELY going to need a db to scale this up
-        let clientList = clients[req.params.room]
-        if (clientList.length >= 2) {
+        if (clients[req.params.room].length >= 2) {
             res.send('Sorry, this game is already in progress.')
         } else {
-            clientList.push(req.originalUrl)
             send(res, 'game.html')
     }} else {
         res.send('Couldn\'t find that room!')
