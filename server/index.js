cf37c703221ef11f4ea5dc79d4357d9e2d7511ca
ayush-shrivastava003 <77131808+ayush-shrivastava003@users.noreply.github.com>
2021-10-26 11:51:21 -0400

    game rooms

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ import {fileURLToPath} from 'url'
 const server = express()
 const HTTPServer = http.Server(server)
 const socket = new Server(HTTPServer)
-
 const __dirname = dirname(fileURLToPath(import.meta.url))
+var clients = {'1234': []}
+
 server.use('/assets', express.static(__dirname + '/../assets/'))
 
 function send(res, file) {
@@ -22,6 +23,20 @@ socket.on('connection', (socket) => {
 
 server.get('/', (req, res) => {
     send(res, 'index.html')
+})
+
+server.get('/:room', (req, res) => {
+    let roomID = req.params.room
+    if (req.params.room in clients) { // DEFINITELY going to need a db to scale this up
+        let clientList = clients[req.params.room]
+        if (clientList.length >= 2) {
+            res.send('Sorry, this game is already in progress.')
+        } else {
+            clientList.push(req.originalUrl)
+            send(res, 'game.html')
+    }} else {
+        res.send('Couldn\'t find that room!')
+    }
 })
 
 HTTPServer.listen(3000, ()=> {
