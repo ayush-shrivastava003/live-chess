99115951eb948d06cc294c8c1eb29b5a338982ae
Tristan <tschartman41@gmail.com>
2021-10-27 10:16:45 -0400

    Delete script.js

--- assets/js/script.js
+++
@@ -1,3 +0,0 @@
-
-// in case of a merge conflict, import this script in game.html:
-// <script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
